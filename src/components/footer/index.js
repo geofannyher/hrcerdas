6f5d8626df17e9f447cd6d5d45a3a43d7f28738f
Geofannyher <geofannyher@gmail.com>
2022-10-04 23:39:42 +0700

    add register form

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -78,7 +78,7 @@ const Footer = () => {
           {/* <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" /> */}
           <div className="sm:flex sm:items-center sm:justify-between px-40 py-20">
             <ul>
-              <li class="mb-4">
+              <li className="mb-4">
                 <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0 ">
                   <a
                     href="#"
@@ -154,7 +154,7 @@ const Footer = () => {
                   </a>
                 </div>
               </li>
-              <li class="mb-4">
+              <li className="mb-4">
                 <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
                   © 2022{" "}
                   <a href="https://flowbite.com/" className="hover:underline">
