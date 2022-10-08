83d186b25bba1d7efe3a62a6f97b955cbc37aa8a
Anggito Suryo Baskoro <anggitosuryobaskoro286@gmail.com>
2022-10-08 08:20:34 +0700

    tabel

---
+++ src/components/Cards/CardLineChart.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Chart from "chart.js";
+
+export default function CardLineChart() {
+  React.useEffect(() => {
+    const data = {
+      labels: [
+        'Red',
+        'Blue',
+        'Yellow'
+      ],
+      datasets: [{
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+      }]
+    };
+    // </block:setup>
+    
+    // <block:config:0>
+    const config = {
+      type: 'doughnut',
+      data: data,
+    };
+    // </block:config>
+    
+    module.exports = {
+      actions: [],
+      config: config,
+    };
+
+    var ctx = document.getElementById("donuts-chart").getContext("2d");
+    window.myLine = new Chart(ctx, config);
+  }, []);
+  return (
+    <>
+      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-blueGray-700">
+        <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
+          <div className="flex flex-wrap items-center">
+            <div className="relative w-full max-w-full flex-grow flex-1">
+              <h6 className="uppercase text-blueGray-100 mb-1 text-xs font-semibold">
+                Overview
+              </h6>
+              <h2 className="text-white text-xl font-semibold">Sales value</h2>
+            </div>
+          </div>
+        </div>
+        <div className="p-4 flex-auto">
+          {/* Chart */}
+          <div className="relative h-350-px">
+            <canvas id="donuts-chart"></canvas>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/admin/dashboard.js
+++ src/pages/admin/dashboard.js
@@ -1,3 +1,6 @@
+
+// import CardDonut from "../../components/Cards/CardDonuts";
+// import CardLineChart from "../../components/Cards/CardLineChart";
 import CardTablePelamar from "../../components/Cards/CardTablePelamar";
 import HeaderStats from "../../components/HeaderStats.js/HeaderStasts";
 
@@ -5,12 +8,15 @@ export default function Dashboard() {
     return (
         <>
             <div className="flex flex-wrap">
-                <div className="w-full mb-12 px-4">
+                <div className="w-full px-4">
                     <HeaderStats />
                 </div>
-                <div className="w-full mb-12 px-4">
+                <div className="w-full mb-12 px-12 xl:w-12/12 items-center">
                     <CardTablePelamar />
                 </div>
+                {/* <div className="w-full mb-12 px-4 xl:w-3/12">
+                    <CardLineChart />
+                </div> */}
             </div>
         </>
     )
