210af21de3e3f5d638040304a56ddf558a78c251
Anggito Suryo Baskoro <anggitosuryobaskoro286@gmail.com>
2022-10-13 15:58:05 +0700

    carddetail

--- src/components/Cards/User/CardDetailUser.js
+++ src/components/Cards/User/CardDetailUser.js
@@ -86,11 +86,19 @@ export default function Detail() {
                             </a>
                         </div>
                     </div>
-                    <div>
-                        <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Last name</label>
-                        <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" disabled />
+                    <div className="flex flex-wrap">
+                        <div className=" xl:w-3/12 px-2">
+                            <img class="w-40 h-35 rounded" src="https://law.uii.ac.id/wp-content/uploads/2021/11/logo-bangkit-2022.png" alt="Default avatar" />
+                        </div>
+                        <div className=" xl:w-9/12 px-2">
+                            <p className="font-semibold text-xl">Bangkit Academy</p>
+                            <p className="font-normal text-gray-400 text-xs">2022</p>
+                            <a type="button" href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 mt-2 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Choose plan
+                                <svg aria-hidden="true" class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </a>
+                        </div>
                     </div>
-
                 </div>
 
 
