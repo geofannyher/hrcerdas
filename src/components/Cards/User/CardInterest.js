0da72d4e4586b167b98d5dec7f3609da5be93494
Anggito Suryo Baskoro <anggitosuryobaskoro286@gmail.com>
2022-10-20 02:24:50 +0700

    chart

---
+++ src/components/Cards/User/CardInterest.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function CardInterest() {
+    return (
+        <div className='relative flex flex-col min-w-0 break-words w-full mb-6 mt-8 shadow-md rounded p-4'>
+            <div className='m-4 mb-20'>
+                <p className='text-left text-xl font-bold text-gray-500'>Interest</p>
+                <div class="mb-1 text-base font-medium dark:text-white">Sport</div>
+                <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
+                    <div class="bg-gray-600 h-2.5 rounded-full dark:bg-gray-300 w-20" ></div>
+                </div>
+                <div class="mb-1 text-base font-medium text-blue-700 dark:text-blue-500">Fashion</div>
+                <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
+                    <div class="bg-blue-600 h-2.5 rounded-full w-88"></div>
+                </div>
+                <div class="mb-1 text-base font-medium text-red-700 dark:text-red-500">Traveling</div>
+                <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
+                    <div class="bg-red-600 h-2.5 rounded-full dark:bg-red-500 w-70" ></div>
+                </div>
+                <div class="mb-1 text-base font-medium text-green-700 dark:text-green-500">Food</div>
+                <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
+                    <div class="bg-green-600 h-2.5 rounded-full dark:bg-green-500 w-80"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CardInterest
