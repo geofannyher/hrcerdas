525b6646ea663d988125cb2534b6e14dda7a6461
Anggito Suryo Baskoro <anggitosuryobaskoro286@gmail.com>
2022-10-20 01:00:39 +0700

    chart-analytic

---
+++ src/components/Cards/User/CardPhotosAnalytic.js
@@ -0,0 +1,19 @@
+// import { mdiMapMarker } from '@mdi/js';
+
+export default function PhotoProfileAnalytic() {
+    return (
+        <>
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-md rounded-lg bg-blueGray-100 border-0 mt-8">
+
+                    <img className="w-40 h-40 rounded-full self-center mt-8" src="https://cdn1-production-images-kly.akamaized.net/Sn5EjcEhFI-jWocCljrP7CZRwyw=/1200x900/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/1439641/original/042027300_1482131661-reddit.jpg" alt="Rounded avatar" />
+                    <h4 className="text-center font-bold mt-4">Hanif Satrio Rimamtomo <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">She/Her</span></h4>
+                    <p className="text-center font-semibold text-sm text-gray-400">Jl Matrip No. 35 Pancoran Bondowoso</p>
+                    <p className="text-center font-semibold text-sm text-gray-400">Mahasiswa</p>
+                    <p className="text-center font-semibold text-sm text-gray-400">Jember, Jawa Timur, Indonesia</p>
+                    <p className="text-center font-semibold text-sm text-gray-800 mb-10">@HanifSatrio</p>
+
+
+            </div>
+        </>
+    )
+}
---
+++ src/pages/admin/analytic.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PhotoProfileAnalytic from "../../components/Cards/User/CardPhotosAnalytic";
+import CardScore from "../../components/Cards/User/CardScore";
+import Chart from "../../components/Cards/User/CardSentiment";
+import CardSosialMedia from "../../components/Cards/User/CardSosialMedia";
+import CardTolerant from "../../components/Cards/User/CardTolerant";
+
+
+export default function Analytic() {
+    return (
+        <>
+            <div className="flex flex-wrap px-12">
+                <div className="w-full mb-12 px-2 xl:w-4/12 items-center">
+                    <PhotoProfileAnalytic />
+                </div>
+                <div className="w-full mb-12 px-12 xl:w-4/12 items-center">
+                    <CardScore />
+                </div>
+                <div className="w-full mb-12 px-4 xl:w-4/12">
+                    <CardSosialMedia />
+                </div>
+                <div className="w-full mb-12 px-4 xl:w-8/12">
+                    <Chart />
+                </div>
+                <div className="w-full mb-12 px-4 xl:w-4/12">
+                    <CardTolerant />
+                </div>
+            </div>
+        </>
+    )
+}
