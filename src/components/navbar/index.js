885674cee534d8fa464d3c4d030a18c5fc05cc2a
Geofannyher <geofannyher@gmail.com>
2022-10-21 04:10:24 +0700

    update layout

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,39 +1,42 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Detailmenu from "./detailmenu";
 import Notification from "./notification";
 const Navbar = () => {
-
+  const navigate = useNavigate()
   const [open, setOpen] = useState(false);
   const [notification, setNotification] = useState(false);
   return (
     <>
       <header>
-        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 shadow-sm rounded dark:bg-gray-900">
+        <nav className="bg-white relativ md:ml-64  border-gray-200 px-2 sm:px-4 py-2.5 shadow-sm rounded dark:bg-gray-900">
           <div className="container flex flex-wrap justify-between items-center mx-auto">
-            <Link to="/" className="flex items-center">
-              <img
-                src={`${process.env.PUBLIC_URL}/HRCerdas.png`}
-                className="mr-3 h-6 sm:h-9"
-                alt="HRCerdas Logo"
-              />
-  
-            </Link>
+            <img
+              onClick={() => { navigate("/") }}
+              src={`${process.env.PUBLIC_URL}/HRCerdas.png`}
+              className="mr-3 h-6 sm:h-9"
+              alt="HRCerdas Logo"
+            />
             <div className="flex items-center lg:order-2">
               {!sessionStorage.getItem("data") ? (
                 <div className="flex justify-center">
-                  <Link
-                    to="/signin"
-                    className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-                  >
-                    Sign In
-                  </Link>
-                  <Link
-                    to="/signup"
-                    className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-                  >
-                    Sign Up
-                  </Link>
+                  <div className="mx-2">
+                    <button
+                      onClick={() => { navigate("/login") }}
+                      type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+                  </div>
+                  <div className="mx-2">
+                    <button
+                      onClick={() => { navigate("/login") }}
+                      type="button" className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">Register</button>
+                  </div>
+                  {/* <div>
+                    <a type="button" href="/register" className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">Register</a>
+                  </div> */}
+                  <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                  </button>
                 </div>
               ) :
                 <div className="flex md:order-2">
