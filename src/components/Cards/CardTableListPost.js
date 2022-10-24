8cdd9b1cc22a4da9f404d223acdd23194dbf1a2a
faiziqbald <faiziqbald@gmail.com>
2022-10-24 11:09:59 +0700

    fixing-delete
    fixing delete function

--- src/components/Cards/CardTableListPost.js
+++ src/components/Cards/CardTableListPost.js
@@ -180,13 +180,6 @@ export default function CardTableListPost() {
                                                     </div>
                                                 </div> ) : null}
 
-
-
-
-
-                                            {/* {openModal && <Delete closeModal={setOpenModal}/> } */}
-                                            <Delete open={openModal} onCLose={() => setOpenModal(false)} />
-
                                             <a href={`/admin/listpost/detailPost/${val._id}`}>
                                                 <button type="button" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">Detail</button>
                                             </a>
