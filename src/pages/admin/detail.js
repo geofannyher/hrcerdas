a7aedf6a90b9d48d7bcb97393411c2e499b69829
Anggito Suryo Baskoro <anggitosuryobaskoro286@gmail.com>
2022-10-27 14:35:43 +0700

    detail

--- src/pages/admin/detail.js
+++ src/pages/admin/detail.js
@@ -84,23 +84,29 @@ export default function DetailUser() {
                             </div>
                         </div>
                         <img className="w-40 h-40 rounded-full self-center" src="https://cdn1-production-images-kly.akamaized.net/Sn5EjcEhFI-jWocCljrP7CZRwyw=/1200x900/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/1439641/original/042027300_1482131661-reddit.jpg" alt="Rounded avatar" />
-                        <h4 className="text-center font-bold mt-4">{profile.name.first_name} {profile.name.last_name} <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">She/Her</span></h4>
+                        <h4 className="text-center font-bold mt-4">{profile.name.first_name} {profile.name.last_name}   
+                            {profile.DetailProfil.gender !== "laki - laki" ? (
+                                 <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">She/Her</span>
+                            ) : (
+                                 <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">He/Him</span>
+                            )}
+                        </h4>
                         {profile.DetailProfil !== undefined ? (
                             <div>
-                                <p className="text-center font-semibold text-sm text-gray-400">{profile.DetailProfil.alamat}</p>
-                                <p className="text-center font-semibold text-sm text-gray-400">{profile.DetailProfil.}</p>
-                                <p className="text-center font-semibold text-sm text-gray-400">Mahasiswa</p>
+                                <p className="text-center font-semibold text-sm text-gray-400">{profile.DetailProfil.location},{profile.DetailProfil.nationality}</p>
+                                <p className="text-center font-semibold text-sm text-gray-400">{profile.DetailProfil.residentialStatus}</p>
+                                <p className="text-center font-semibold text-sm text-gray-800 m-2">{profile.aboutme}</p>
                             </div>
 
                         ) : (
                             <div>
-                                <p className="text-center font-semibold text-sm text-gray-400">{profile.DetailProfil.alamat}</p>
                                 <p className="text-center font-semibold text-sm text-gray-400">Jember, Jawa Timur, Indonesia</p>
                                 <p className="text-center font-semibold text-sm text-gray-400">Mahasiswa</p>
+                                <p className="text-center font-semibold text-sm text-gray-800">Deskription</p>
                             </div>
 
                         )}
-                        <p className="text-center font-semibold text-sm text-gray-800">@HanifSatrio</p>
+
                         <a href="/admin/profile/edit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none sm:w-auto m-6 focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Accept</a>
                         <a href="/admin/profile/edit" className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none sm:w-auto m-6 focus:ring-red-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 mt-1 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Denied</a>
                     </div>
