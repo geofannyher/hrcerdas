5d01b7f5a6075f14127d83fdf1f6df4760012671
Dian Buana Putra <dianbuana2069@gmail.com>
2022-11-03 15:00:53 +0700

    loading screen not done

--- src/components/spinner/index.js
+++ src/components/spinner/index.js
@@ -1,8 +1,10 @@
-import React from 'react'
-
 export const Spinner = () => {
+    // $(window).on('load',function(){
+    //     $(".Spinner").fadeOut(600);
+    //     $(".App").fadeIn(600);
+    // });
     return (
-        <div className="preloader loaded-success fixed inset-0 z-50 bg-gray-50">
+        <div className="duration-100 preloader loaded-success fixed inset-0 z-50 bg-gray-50">
             <div className="absolute left-1/2 top-1/2 transform -translate-y-1/2">
                 <div className="relative mx-auto my-12">
                     <div className="inline-block">
@@ -16,4 +18,6 @@ export const Spinner = () => {
         </div>
     )
 }
+
+
 export default Spinner
